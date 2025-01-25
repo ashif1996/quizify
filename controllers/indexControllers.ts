46de084e169ffb2504644116637ecc73e3a257d1
Ashif Hassan <ashifhassan1996@gmail.com>
2025-01-25 13:51:03 +0530

    update index controller functions

--- controllers/indexControllers.ts
+++ controllers/indexControllers.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 
 import httpStatusCodes from "../utils/httpStatusCodes.js";
 
-const getHome = (req: Request, res: Response): void => {
+const getHome = (req: Request, res: Response) => {
     const locals = { title: "Home | Quizify" };
     res.status(httpStatusCodes.OK).render("index", {
         locals,
@@ -10,7 +10,7 @@ const getHome = (req: Request, res: Response): void => {
     });
 };
 
-const getContact = (req: Request, res: Response): void => {
+const getContact = (req: Request, res: Response) => {
     const locals = { title: "Contact Us | Quizify" };
     res.status(httpStatusCodes.OK).render("contact", {
         locals,
