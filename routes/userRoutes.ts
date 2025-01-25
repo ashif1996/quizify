0719af0803730613cb4c336fc74a9d652984c77a
Ashif Hassan <ashifhassan1996@gmail.com>
2025-01-25 13:56:20 +0530

    add new routes related to quiz

--- routes/userRoutes.ts
+++ routes/userRoutes.ts
@@ -2,6 +2,7 @@ import express from "express";
 const router = express.Router();
 
 import userControllers from "../controllers/userControllers.js";
+import jwtMiddleware from "../middlewares/jwtMiddleware.js";
 
 router.route("/login")
     .get(userControllers.getUserLogin)
@@ -14,8 +15,17 @@ router.route("/signup")
     .post(userControllers.userSignup);
 
 router.get("/verify-email", userControllers.verifyEmail);
-router.get("/resend-verification-email", userControllers.resendVerificationEmail);
 
-router.get("/user-profile", userControllers.getUserProfile);
+router.post("/resend-verification-email", jwtMiddleware, userControllers.resendVerificationEmail);
+
+router.get("/user-profile", jwtMiddleware, userControllers.getUserProfile);
+
+router.get("/quiz-history", jwtMiddleware, userControllers.getQuizHistory);
+
+router.get("/quiz-options", jwtMiddleware, userControllers.selectQuizOptions);
+
+router.post("/quiz-questions", jwtMiddleware, userControllers.getQuizQuestions);
+
+router.post("/submit-quiz", jwtMiddleware, userControllers.submitQuiz);
 
 export default router;
